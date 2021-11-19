938a29497b9a25530f31d86b89b955c904fb6d7b
Moonpearl <romain.tello@gmail.com>
2021-11-19 10:40:26 +0100

    Add migration and fixtures

--- scripts/migrate.mjs
+++ scripts/migrate.mjs
@@ -2,11 +2,30 @@ import faunadb from 'faunadb';
 import faunaClient from './fauna-client.mjs';
 const fql = faunadb.query;
 
-const userData = [
-  { firstName: 'Jean-Pierre', lastName: 'Martin', email: 'jeanpierre.martin@test.com' },
-  { firstName: 'Marie-Madeleine', lastName: 'Michel', email: 'mariemadeleine.michel@test.com' },
-  { firstName: 'Charles-Henri', lastName: 'Lefevre', email: 'charleshenri.lefevre@test.com' },
-];
+const createCollectionUnlessExists = (name) =>
+  faunaClient.query(
+    fql.If(
+      fql.Exists(
+        fql.Collection(name)
+      ),
+      null,
+      fql.CreateCollection({ name })
+    )
+  );
+
+const createAllIndexUnlessExists = (name, collectionName) =>
+  faunaClient.query(
+    fql.If(
+      fql.Exists(
+        fql.Index(name)
+      ),
+      null,
+      fql.CreateIndex({
+        name,
+        source: fql.Collection(collectionName),
+      })
+    )
+  )
 
 // This script migrates the database schema to the database server
 const run = async () => {
@@ -14,39 +33,72 @@ const run = async () => {
   console.info('Connecting to database...');
     
   console.info('Migrating schema...')
-  await faunaClient.query(
-    fql.If(
-      fql.Exists(
-        fql.Collection('User')
-      ),
-      null,
-      fql.CreateCollection({ name: 'User' })
-    )
-  );
+  console.info('Creating Room collection...')
+  await createCollectionUnlessExists('Room')
+  await createAllIndexUnlessExists('all_rooms', 'Room')
 
+  console.info('Creating Direction collection...')
+  await createCollectionUnlessExists('Direction')
+  await createAllIndexUnlessExists('all_directions', 'Direction')
+
+  console.info('Creating RoomConnection collection...')
+  await createCollectionUnlessExists('RoomConnection')
+  await createAllIndexUnlessExists('all_room_connections', 'RoomConnection')
+
+  console.info('Creating room_by_name index...')
   await faunaClient.query(
     fql.If(
       fql.Exists(
-        fql.Index('all_users')
+        fql.Index('room_by_name')
       ),
       null,
       fql.CreateIndex({
-        name: 'all_users',
-        source: fql.Collection('User')
+        name: 'room_by_name',
+        source: fql.Collection('Room'),
+        terms: [
+          { field: ['data', 'name'] },
+        ],
+        unique: true,
       })  
     )
   );
 
-  for (let data of userData) {
-    await faunaClient.query(
-      fql.Create(
-        fql.Collection('User'),
-        {
-          data
-        }
-      )
+  console.info('Creating direction_by_command index...')
+  await faunaClient.query(
+    fql.If(
+      fql.Exists(
+        fql.Index('direction_by_command')
+      ),
+      null,
+      fql.CreateIndex({
+        name: 'direction_by_command',
+        source: fql.Collection('Direction'),
+        terms: [
+          { field: ['data', 'command'] },
+        ],
+        unique: true,
+      })  
     )
-  }
+  );
+
+  console.info('Creating room_connection_by_from_room_and_direction...')
+  await faunaClient.query(
+    fql.If(
+      fql.Exists(
+        fql.Index('room_connection_by_from_room_and_direction')
+      ),
+      null,
+      fql.CreateIndex({
+        name: 'room_connection_by_from_room_and_direction',
+        source: fql.Collection('RoomConnection'),
+        terms: [
+          { field: ['data', 'fromRoom'] },
+          { field: ['data', 'direction'] },
+        ],
+        unique: true,
+      })
+    )
+  )
 }
 
 // Run main process
